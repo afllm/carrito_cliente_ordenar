18830bf00127da61971bc269236be2169018fc21
afllm <afllm007@gmail.com>
2018-11-03 12:29:32 +0100

    Selector de registros por página en usuario 03-11-2018

--- public_html/js/app/usuario/plist.js
+++ public_html/js/app/usuario/plist.js
@@ -2,6 +2,8 @@
 
 moduleUsuario.controller('usuarioPlistController', ['$scope', '$http', '$location', 'toolService', '$routeParams',
     function ($scope, $http, $location, toolService, $routeParams) {
+        
+        $scope.rpps = [10,20,50,500];
         $scope.totalPages = 1;
         if (!$routeParams.rpp) {
             $scope.rpp = 10;
@@ -26,7 +28,7 @@ moduleUsuario.controller('usuarioPlistController', ['$scope', '$http', '$locatio
             $scope.ajaxDataUsuariosNumber = response.data.message;
             $scope.totalPages = Math.ceil($scope.ajaxDataUsuariosNumber / $scope.rpp);
             $scope.list = [];
-            for (var i = 1; i <= $scope.totalPages; i++) {
+            for (var i = 1; i <$scope.totalPages; i++) {
                 $scope.list.push(i);
             }
         }, function (response) {
@@ -44,6 +46,22 @@ moduleUsuario.controller('usuarioPlistController', ['$scope', '$http', '$locatio
             $scope.ajaxDataUsuarios = response.data.message || 'Request failed';
             $scope.status = response.status;
         });
+        
+        $scope.change = function(){
+                        $http({
+                method: 'GET',
+                url: 'http://localhost:8081/trolleyes/json?ob=usuario&op=getpage&rpp=' + $scope.rpp + '&page=' + $scope.page
+            }).then(function (response) {
+                $location.path('usuario/plist/'+$scope.rpp+'/'+ $scope.page);
+                $scope.status = response.status;
+                $scope.ajaxDataUsuarios = response.data.message;
+            }, function (response) {
+                $scope.ajaxDataUsuarios = response.data.message || 'Request failed';
+                $scope.status = response.status;
+            });
+        }
+        
+        
         $scope.isActive = toolService.isActive;
     }
 ]);
